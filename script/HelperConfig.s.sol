b55800b4906e144f31a335320c63cc853b6af851
worksofallwin <princeallwin199@gmail.com>
2023-08-04 18:09:48 +0530

    Complete HelperConfig.sol

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address weth;
+        address wbtc;
+        address wethUsdPriceFeedAddress;
+        address wbtcUsdPriceFeedAddress;
+        uint256 deployerKey;
+    }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = _sepoliaConfig();
+        } else {
+            activeNetworkConfig = _anvilConfig();
+        }
+    }
+
+    function _sepoliaConfig() private view returns (NetworkConfig memory) {
+        NetworkConfig memory config = NetworkConfig({
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            wethUsdPriceFeedAddress: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeedAddress: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            deployerKey: vm.envUint("SEPOLIA_PRIVATE_KEY")
+        });
+        return config;
+    }
+
+    function _anvilConfig() private returns (NetworkConfig memory) {
+        if (activeNetworkConfig.weth != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        ERC20Mock wethMock = new ERC20Mock();
+        ERC20Mock wbtcMock = new ERC20Mock();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory config = NetworkConfig({
+            weth: address(wethMock),
+            wbtc: address(wbtcMock),
+            wethUsdPriceFeedAddress: address(ethUsdPriceFeed),
+            wbtcUsdPriceFeedAddress: address(btcUsdPriceFeed),
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+        return config;
+    }
+}
